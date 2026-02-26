1c8fd6d633f87576434e7c0af55b34d03d9e08b8
emcombs23 <emcombs.23.sv@gmail.com>
2026-02-26 17:39:49 +0000

    up to data

--- Web Scraping/models.py
+++ Web Scraping/models.py
@@ -12,12 +12,26 @@ class Bio(SQLModel, table=True):
     highSchool: str | None = None
 
 class Stats(SQLModel, table=True):
-    firstName: str = Field(primary_key=True)
-    lastName: str = Field(primary_key=True)
-    gamesPlayed: int | None = None
-    goals: int | None = None
-    assists: int | None = None
-    points: int | None = None
-    plusMinus: int | None = None
-    penaltyMinutes: int | None = None
-
+	Number: Optional[int] = Field(default=None, description="Jersey number")
+	FirstName: str = Field(primary_key=True, foreign_key="bio.FirstName")
+	LastName: str = Field(primary_key=True, foreign_key="bio.LastName")
+	GP: Optional[int] = None
+	G: Optional[int] = None
+	A: Optional[int] = None
+	PTS: Optional[int] = None
+	SH: Optional[int] = None
+	SH_PCT: Optional[float] = None
+	plus_minus: Optional[int] = None
+	PPG: Optional[int] = None
+	SHG: Optional[int] = None
+	FG: Optional[int] = None
+	GWG: Optional[int] = None
+	GTG: Optional[int] = None
+	OTG: Optional[int] = None
+	HTG: Optional[int] = None
+	UAG: Optional[int] = None
+	PN_PIM: Optional[str] = Field(default=None, description="Penalty - PIM (raw)")
+	MIN: Optional[int] = None
+	MAJ: Optional[int] = None
+	OTH: Optional[int] = None
+	BLK: Optional[int] = None
